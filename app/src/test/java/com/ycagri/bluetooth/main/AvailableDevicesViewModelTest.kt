e0f5fa57f72a751bcf2f304d4f84cbd8ac5c2876
Yigit Cagri Akkaya <akkayacagri@gmail.com>
2021-05-02 14:19:42 +0300

    New test added to available devices viewmodel.

--- app/src/test/java/com/ycagri/bluetooth/main/AvailableDevicesViewModelTest.kt
+++ app/src/test/java/com/ycagri/bluetooth/main/AvailableDevicesViewModelTest.kt
@@ -1,8 +1,12 @@
 package com.ycagri.bluetooth.main
 
+import android.bluetooth.BluetoothDevice
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.ycagri.bluetooth.datasource.DataRepository
 import com.ycagri.bluetooth.main.viewmodel.AvailableDevicesViewModel
+import com.ycagri.bluetooth.util.TestUtil
 import com.ycagri.bluetooth.util.mock
 import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.notNullValue
@@ -44,5 +48,21 @@ class AvailableDevicesViewModelTest {
         viewModel.devices.observeForever(mock())
         viewModel.setDiscovering(mock(), true)
         verify(repository).getAvailableDevices(any())
+    }
+
+    @Test
+    fun testRetrieveAvailableDevicesToUI() {
+        val devices = TestUtil.createDevices(10, "Test Device", "test_address")
+        `when`(repository.getAvailableDevices(any())).thenReturn(MutableLiveData(devices))
+
+        val observer = mock<Observer<List<BluetoothDevice>>>()
+        viewModel.devices.observeForever(observer)
+        viewModel.setDiscovering(mock(), true)
+        verify(repository).getAvailableDevices(any())
+
+        for (i in 0 until 10) {
+            Assert.assertEquals(devices[i].name, viewModel.devices.value?.get(i)?.name)
+            Assert.assertEquals(devices[i].address, viewModel.devices.value?.get(i)?.address)
+        }
     }
 }
